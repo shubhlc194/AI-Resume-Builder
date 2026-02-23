518e9b8074063f1c455b8c9f7c4d9a2618844e8d
shubhlc194 <shubhamkc194@gmail.com>
2026-02-23 22:23:23 +0530

    add dashobard

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const DashBoard = () => {
   return (
-    <div>
-      DashBoard
+    <div className='p-10 md:px-20 lg:px-32'>
+      <h2 className='font-bold text-3xl'> My Resume</h2>
+      <p>Start Creating AI-Resume to Your Next Job Role</p>
+      <div>
+        
+      </div>
     </div>
   )
 }
